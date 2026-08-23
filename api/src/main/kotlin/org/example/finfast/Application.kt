b928d7645960110b5f2e746a4ad25ff3c96749d6
Fiodar <36445233+yatskevichfyodor@users.noreply.github.com>
2026-08-23 13:30:01 +0300

    fix api

--- api/src/main/kotlin/org/example/finfast/Application.kt
+++ api/src/main/kotlin/org/example/finfast/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TransactionApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<TransactionApplication>(*args)
+    runApplication<Application>(*args)
 }
