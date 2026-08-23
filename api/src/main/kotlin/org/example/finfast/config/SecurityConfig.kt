59d052e9617c8ffe1c0b787e60180021d1cb23f0
Fiodar <36445233+yatskevichfyodor@users.noreply.github.com>
2026-08-23 14:56:40 +0300

    fix api

---
+++ api/src/main/kotlin/org/example/finfast/config/SecurityConfig.kt
@@ -0,0 +1,22 @@
+package org.example.org.example.finfast.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .cors { }
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.anyRequest().permitAll()
+            }
+
+        return http.build()
+    }
+}
