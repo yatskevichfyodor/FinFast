667f362c12c05145832d43ea07a999dd89c8b755
Fiodar <36445233+yatskevichfyodor@users.noreply.github.com>
2026-08-28 20:41:17 +0300

    UI: fix PWA config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vuetify from 'vite-plugin-vuetify'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,42 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     vuetify({ autoImport: true }),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      includeAssets: [
+        'favicon.png',
+        'byn-symbol.webp'
+      ],
+
+      manifest: {
+        name: 'FinFast',
+        short_name: 'FinFast',
+        description: 'Персональный трекер расходов',
+        theme_color: '#4a7ba7',
+        background_color: '#ffffff',
+        display: 'standalone',
+
+        icons: [
+          {
+            src: 'favicon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'favicon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+
+      workbox: {
+        globPatterns: [
+          '**/*.{js,css,html,ico,png,webp,svg}'
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
