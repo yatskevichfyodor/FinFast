a51732789ae7077113c409c96346fe55f173369c
Fiodar <36445233+yatskevichfyodor@users.noreply.github.com>
2026-09-08 19:14:46 +0300

    UI: fix

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -54,8 +54,7 @@ router.beforeEach(to => {
   const isAuthenticated = localStorage.getItem('finfast-access-token') !== null || (
     localStorage.getItem('finfast-offline-mode') === 'true' &&
     localStorage.getItem('finfast-user-id') !== null
-  ) || localStorage.getItem('finfast-anonymous-profile') !== null &&
-    localStorage.getItem('finfast-offline-mode') === 'true'
+  )
 
   if (to.meta.requiresAuth && !isAuthenticated) {
     return { name: 'login', query: { redirect: to.fullPath } }
