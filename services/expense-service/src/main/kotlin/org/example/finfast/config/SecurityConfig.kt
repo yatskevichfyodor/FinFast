ca63a0df93b9067d571f83294d55a0a1c9929ae0
Fiodar <36445233+yatskevichfyodor@users.noreply.github.com>
2026-09-11 00:44:04 +0300

    API: fix structure

--- services/expense-service/src/main/kotlin/org/example/finfast/config/SecurityConfig.kt
+++ services/expense-service/src/main/kotlin/org/example/finfast/config/SecurityConfig.kt
@@ -1,4 +1,4 @@
-package org.example.finfast.auth.config
+package org.example.finfast.config
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
