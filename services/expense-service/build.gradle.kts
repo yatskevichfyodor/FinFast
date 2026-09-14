1f80cacbe60fe5e7f58afe25346ff63a56c8c622
Fiodar <36445233+yatskevichfyodor@users.noreply.github.com>
2026-09-14 20:50:08 +0300

    EXPENSE API: fix kafka

--- services/expense-service/build.gradle.kts
+++ services/expense-service/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-flyway")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.boot:spring-boot-starter-kafka")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("tools.jackson.module:jackson-module-kotlin")
     runtimeOnly("org.postgresql:postgresql")
